81ab0d05dd6b25a1e174d668d2571a845efa2f9d
Michael Bach <github@anti.cx>
2023-02-03 06:23:57 +0100

    fix: move autoexec_custom creation to postinstall

---
+++ .regelschirm_config_postinstall.ps1
@@ -0,0 +1,15 @@
+# create autoexec_custom if it doesn't exist
+if (!(Test-Path "$PSScriptRoot\autoexec_custom.cfg")) {
+    @(
+        '// main settings/preferences',
+        'sensitivity "1"',
+        'zoom_sensitivity_ratio_mouse "1"',
+        'volume "0.70"',
+        'voice_mixer_volume "0.8"',
+        'voice_scale "0.7"',
+        'exec ch_draken',
+        'exec vm_dev1ce'
+    ) | % {
+        Add-Content -Path "autoexec_custom.cfg" -Value $_
+    }
+}
--- _update_regelschirm_config.ps1
+++ _update_regelschirm_config.ps1
@@ -27,18 +27,4 @@ Invoke-WebRequest https://github.com/niederschlag/regelschirm_config/archive/ref
 Unzip -f "$PSScriptRoot\config.zip"
 rm config.zip
 
-# create autoexec_custom if it doesn't exist
-if (!(Test-Path "$PSScriptRoot\autoexec_custom.cfg")) {
-    @(
-        '// main settings/preferences',
-        'sensitivity "1"',
-        'zoom_sensitivity_ratio_mouse "1"',
-        'volume "0.70"',
-        'voice_mixer_volume "0.8"',
-        'voice_scale "0.7"',
-        'exec ch_draken',
-        'exec vm_dev1ce'
-    ) | % {
-        Add-Content -Path "autoexec_custom.cfg" -Value $_
-    }
-}
+& "$PSScriptRoot\.regelschirm_config_postinstall.ps1"
