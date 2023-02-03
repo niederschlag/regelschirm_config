73669e1fef4b71762bb6f247609a6a77b165df82
Michael Bach <github@anti.cx>
2023-02-03 06:16:53 +0100

    fix: autocreate autoexec_custom if it doesn't exist

--- _update_regelschirm_config.ps1
+++ _update_regelschirm_config.ps1
@@ -26,3 +26,19 @@ function Unzip {
 Invoke-WebRequest https://github.com/niederschlag/regelschirm_config/archive/refs/heads/master.zip -OutFile config.zip
 Unzip -f "$PSScriptRoot\config.zip"
 rm config.zip
+
+# create autoexec_custom if it doesn't exist
+if (!(Test-Path "$PSScriptRoot\autoexec_custom.cfg")) {
+    @(
+        '// main settings/preferences',
+        'sensitivity "1"',
+        'zoom_sensitivity_ratio_mouse "1"',
+        'volume "0.70"',
+        'voice_mixer_volume "0.8"',
+        'voice_scale "0.7"',
+        'exec ch_draken',
+        'exec vm_dev1ce'
+    ) | % {
+        Add-Content -Path "autoexec_custom.cfg" -Value $_
+    }
+}
